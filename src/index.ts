45c29200dfa91d455130bde720b38a72820463a0
Marcin S <marcin@bytedude.com>
2021-03-11 19:34:34 +0100

    Harden comms with connector window and update API

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@
 
 import { ChildHandshake, WindowMessenger } from "post-me";
 import type { Connection } from "post-me";
-import type { ProviderMetadata, SkappInfo } from "skynet-interface-utils";
+import { emitStorageEvent, listenForStorageEvent, monitorOtherListener, ProviderMetadata, SkappInfo } from "skynet-interface-utils";
 
-export abstract class Provider<T> {
+export abstract class BaseProvider<T> {
   isProviderConnected: boolean;
   methods: {
     [index: string]: Function;
@@ -56,41 +56,70 @@ export abstract class Provider<T> {
     return this.methods[method]();
   }
 
+  /**
+   * Connects by waiting for connection info from the connector
+   *
+   * 1. The router has already launched the connector and the bridge is now calling connectPopup() on the provider.
+   *
+   * 2. The provider waits for the connection info from the connector.
+   *
+   * 3. The provider sets the skapp as connected and successfully resolves the promise.
+   */
   protected async connectPopup(skappInfo: SkappInfo): Promise<void> {
-    // Wait for connection info from the connector.
+    // Event listener that waits for connection info from the connector.
+    const { promise: promiseConnectionInfo, controller: controllerConnectionInfo } = listenForStorageEvent(
+      "connector-connection-info"
+    );
+    // Kick off another event listener along with the first one as the connector window may still be closed or an error may occur, and we need to handle that.
+    const { promise: promiseLong, controller: controllerLong } = listenForStorageEvent("connector");
+    // Start the connector pinger.
+    const { promise: promisePing, controller: controllerPing } = monitorOtherListener(
+      "provider",
+      "connector",
+      5000
+    );
 
-    const receivedConnectionInfo: string = await new Promise((resolve, reject) => {
-      const handleEvent = ({ key, newValue }: StorageEvent) => {
-        window.removeEventListener("storage", handleEvent);
+    const promise: Promise<void> = new Promise(async (resolve, reject) => {
+      // Make this promise run in the background and reject on window close or any errors.
+      promiseLong.catch((err: string) => {
+        // Don't emit an error to the connector, it should close on its own on error.
+        reject(err);
+      });
+      promisePing.catch(() => {
+        reject("Connector timed out");
+      });
 
-        if (!key || !newValue) {
-          reject("Storage event data not found");
-          return;
-        }
-        window.localStorage.removeItem(key);
+      // Wait for connection info from the connector.
 
-        if (key === "success") {
-          resolve(newValue);
-        } else if (key === "closed") {
-          reject("Window was closed");
-        } else {
-          // Key should be 'error'.
-          if (key !== "error") {
-            reject("Unknown key received");
-          }
-          reject(newValue);
-        }
-      };
+      let connectionInfo;
+      try {
+        const receivedConnectionInfo = await promiseConnectionInfo;
+        connectionInfo = JSON.parse(receivedConnectionInfo);
+        // TODO: Validate the connectionInfo using required abstract function and send an error if invalid.
+      } catch (err) {
+        // Send an error to the connector before throwing.
+        emitStorageEvent("provider", "error", err);
+        reject(err);
+        return;
+      }
 
-      window.addEventListener("storage", handleEvent);
+      // Set the skapp as connected.
+
+      await this.saveConnectionInfo(connectionInfo);
+      this.isProviderConnected = true;
+      resolve();
     });
-    const connectionInfo = JSON.parse(receivedConnectionInfo);
-    // TODO: Validate the connectionInfo using required abstract function and send an error if invalid.
 
-    // Set the skapp as connected.
-
-    await this.saveConnectionInfo(connectionInfo);
-    this.isProviderConnected = true;
+    return promise
+      .catch((err) => {
+        throw err;
+      })
+      .finally(() => {
+        // Clean up the event listeners and promises.
+        controllerConnectionInfo.cleanup();
+        controllerLong.cleanup();
+        controllerPing.cleanup();
+      });
   }
 
   /**
